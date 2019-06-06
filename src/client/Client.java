3f9534096ce52baa97eae41e2b9b217bcebf7c48
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-06-06 16:24:14 +0430

    add methodes to client.java

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,4 +1,37 @@
 package client;
 
-public class Client {
+import constants.Communication;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.Scanner;
+
+public class Client implements ClientCommunication, Communication {
+
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+    @Override
+    public void commandReader(Scanner jin, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
+
+    }
+
+    @Override
+    public void login(Scanner jin, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
+
+    }
+
+    @Override
+    public void signup(Scanner jin, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
+
+    }
+
+    @Override
+    public void notificationListener() {
+
+    }
 }
