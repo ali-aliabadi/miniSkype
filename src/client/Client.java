45977544994d9dd8f0a59cab4df33a0e146d3be3
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-06-06 17:00:40 +0430

    updating client (add another socket and build to send and get command)

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,37 +1,91 @@
 package client;
 
 import constants.Communication;
+import constants.Constants;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Client implements ClientCommunication, Communication {
 
+    private Socket commandsSocket, notficationSocket;
+    private DataInputStream commandInput, notificationInput;
+    private DataOutputStream commandOutput, notificationOutput;
+    private Thread notifiListener, handelingCommands;
+    private Scanner jin;
 
-    public static void main(String[] args) {
 
-        
+    Client(String address, int port) throws IOException {
 
+        commandsSocket = new Socket(address, port);
+        commandInput = new DataInputStream(commandsSocket.getInputStream());
+        commandOutput = new DataOutputStream(commandsSocket.getOutputStream());
+
+        notficationSocket = new Socket(address, port);
+        notificationInput = new DataInputStream(notficationSocket.getInputStream());
+        notificationOutput = new DataOutputStream(notficationSocket.getOutputStream());
+
+        jin = new Scanner(System.in);
+
+        Runnable r1 = this::notificationListener;
+        Runnable r2 = this::commandReader;
+        notifiListener = new Thread(r1);
+        handelingCommands = new Thread(r2);
+        notifiListener.start();
+        handelingCommands.start();
     }
 
-    @Override
-    public void commandReader(Scanner jin, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
-
-    }
-
-    @Override
-    public void login(Scanner jin, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
-
-    }
-
-    @Override
-    public void signup(Scanner jin, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
-
-    }
 
     @Override
     public void notificationListener() {
 
+    }
+
+    @Override
+    public void login() {
+
+    }
+
+    @Override
+    public void signup() {
+
+    }
+
+    @Override
+    public void commandReader() {
+
+        String command;
+
+        while (true) {
+            System.out.println('*' * 33);
+            System.out.print("please input your command : ");
+            try {
+                commandOutput.writeUTF(jin.nextLine());
+            } catch (IOException e) {
+                System.err.println("error in sending command to server");
+            }
+            commandProcess:
+            while (true) {
+                try {
+                    command = commandInput.readUTF();
+
+                    switch (command) {
+                        case Constants.NEED_INPUT:
+                            commandOutput.writeUTF(jin.nextLine());
+                            break;
+                        case Constants.END_OF_PROCESS:
+                            break commandProcess;
+                        default:
+                            System.out.println(command);
+                    }
+                    
+                } catch (IOException e) {
+                    System.err.println("error in getting command from server and processing it");
+                }
+            }
+        }
     }
 }
