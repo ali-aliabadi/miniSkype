1406a4c2ad27b8a9dd44aa3d1900bb10dc8fb8b0
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-06-06 15:37:50 +0430

    adding unknownCommand method

--- src/server/ClientHandlerCommunication.java
+++ src/server/ClientHandlerCommunication.java
@@ -6,6 +6,7 @@ package server;
  *  commandHandler : handle client commands (like client request for signup)
  *  login : (must have a pair method in client)
  *  signup : (must have a pair method in client)
+ *  unknownCommand : for bad (unexisted) command from client, sending proper answer.
  */
 public interface ClientHandlerCommunication {
 
@@ -16,5 +17,6 @@ public interface ClientHandlerCommunication {
     void login();
     void signup();
 
+    void unknownCommand();
 
 }
