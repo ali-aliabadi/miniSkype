aacb256db8195f84b9448aee0c1c1514d4b19f0e
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-06-06 15:38:13 +0430

    building up threads

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -7,10 +7,11 @@ import java.net.Socket;
 
 public class ClientHandler implements ClientHandlerCommunication {
 
-    Socket socket;
-    DataInputStream dataInputStream;
-    DataOutputStream dataOutputStream;
+    private Socket socket;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
     boolean isLoggedIn = false;
+    private Thread notifiListener, handelingCommands;
 
 
     ClientHandler(Socket socket) {
@@ -23,6 +24,24 @@ public class ClientHandler implements ClientHandlerCommunication {
             System.err.println("error in getting streams");
         }
 
+        Runnable r1 = this::notificationListener;
+        Runnable r2 = this::commandHandler;
+        notifiListener = new Thread(r1);
+        handelingCommands = new Thread(r2);
+        notifiListener.start();
+        handelingCommands.start();
+
+        /*
+          <code>this::notificationListener</code> is equals to below code
+               <code>
+                   new Runnable() {
+                      public void run() {
+                          notificationListener();
+                      }
+                  };
+               </code>
+         */
+
     }
 
 
@@ -34,6 +53,29 @@ public class ClientHandler implements ClientHandlerCommunication {
     @Override
     public void commandHandler() {
 
+        String command;
+
+        try {
+            while (true) {
+                command = dataInputStream.readUTF();
+                switch (command) {
+                    case "login":
+                        login();
+                        break;
+                    case "signuo":
+                    case "sign up":
+                    case "register":
+                        signup();
+                        break;
+                    default:
+                        unknownCommand();
+                        break;
+                }
+            }
+        } catch (IOException ioe) {
+            System.err.println("error in command handler for socket with channel : " + socket.getChannel()
+                    + ", and port number of : " + socket.getPort());
+        }
     }
 
     @Override
@@ -43,6 +85,11 @@ public class ClientHandler implements ClientHandlerCommunication {
 
     @Override
     public void signup() {
+
+    }
+
+    @Override
+    public void unknownCommand() {
 
     }
 }
