e79ea645c9b0df0673dda26b2dc16dd4f78fabc6
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-06-13 15:30:09 +0430

    adding type to constants

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -6,5 +6,6 @@ public class Constants {
     public static final String SIGNUPREQUEST = "signUpReq";
     public static final String NOTIFICATION = "notification";
     public static final String CHATMASSAGE = "chatMassage";
+    public static final String TYPE = "type";
 
 }
