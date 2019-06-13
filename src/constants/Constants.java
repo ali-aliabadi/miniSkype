973ecc075213650112efac1468bc32289e2cb376
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-06-13 12:44:40 +0430

    add constants package for commands type

---
+++ src/constants/Constants.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class Constants {
+
+    public static final String LOGINREQUEST = "loginReq";
+    public static final String SIGNUPREQUEST = "signUpReq";
+    public static final String NOTIFICATION = "notification";
+    public static final String CHATMASSAGE = "chatMassage";
+
+}
